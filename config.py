ee77fb7551025319248d2d01bd69d9885cb0dcb1
helianthus1370@gmail.com <545264likang>
2020-06-23 23:46:41 +0800

    删除冗余以及格式化代码.

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@ Author: HeliantHuS
+@ Codes are far away from bugs with the animal protecting
+@ Time:  6/23/2020
+@ FileName: config.py
+"""
+
 # wechatVersion = "2.6.6.28"
 # wechatOffset = 0x1131B64
 
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@ Author: HeliantHuS
+@ Codes are far away from bugs with the animal protecting
+@ Time:  6/23/2020
+@ FileName: main.py
+"""
+
 import pymem
 import config
 import struct
 import binascii
 
-def getCBytes(password):
+# getCBytes 获得C语言格式的密码
+def getCBytes(password) -> str:
     result = ""
     for i in range(0, len(password), 2):
-        result += "0x" + password[i:i+2] + ", "
-
-
+        result += "0x" + password[i:i + 2] + ", "
 
     return """
         unsigned char pass[] = {%s};
-    """%(result)
-def getPassword(p) -> (str, int, str):
+    """ % (result)
+
+
+# getPassword 获得数据库密码
+def getPassword(p) -> (int, str):
     # 得到这个进程加载的WeChatWin.dll的基地址
     base_address = pymem.process.module_from_name(p.process_handle, "wechatwin.dll").lpBaseOfDll
 
@@ -28,7 +39,9 @@ def getPassword(p) -> (str, int, str):
     result = binascii.b2a_hex(password)
     return base_address, result.decode()
 
-def printResult(base_offset, password):
+
+# printResult 打印最后的结果 (更好看一些
+def printResult(base_offset, password) -> None:
     print(f"""
         当前微信版本: {config.wechatVersion},
         WeChatWin.Dll的基地址为: {hex(base_offset)},
@@ -37,14 +50,13 @@ def printResult(base_offset, password):
         解密数据库的C语言格式密码: {getCBytes(password)}
 
 
-            Enjoy Python!
+            Enjoy Python!  
+                By: HeliantHuS
     """)
+
 
 if __name__ == '__main__':
     p = pymem.Pymem()
     p.open_process_from_name("WeChat.exe")
     base_offset, password = getPassword(p)
     printResult(base_offset, password)
-
-
-
