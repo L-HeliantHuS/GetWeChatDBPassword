8d13b1d254c08c28e1e6b9dfa9b50af8e61c6829
Hel1antHu5 <helianthus1370@gmail.com>
2022-07-04 22:11:03 +0800

    update wechat 3.7.0.30

--- config.py
+++ config.py
@@ -10,8 +10,12 @@
 # wechatVersion = "2.6.6.28"
 # wechatOffset = 0x1131B64
 
-wechatVersion = "2.9.0.123"
-wechatOffset = 0x16B4D50
+# wechatVersion = "2.9.0.123"
+# wechatOffset = 0x16B4D50
 
 # wechatVersion = "2.9.0.112"
 # wechatOffset = 0x16B4C70
+
+wechatVersion = "3.7.0.30"
+wechatOffset = 0x2366524
+
